f646f7aae14c2883f99455a7a59197aa33f703d3
Maxime Chasles <maxime.chasles@inova-software.com>
2017-05-04 23:20:17 +0200

    Add React logo on landing page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {MarvelCharactersList} from './components/MarvelCharactersList/'
 import {MarvelCharacterDetail} from './components/MarvelCharacterDetail/'
 import {loadCharacters} from './lib/marvelApiService'
 import './App.scss';
+import logo from './assets/logo.svg';
 
 class App extends Component {
   state = {
@@ -21,6 +22,11 @@ class App extends Component {
       <Router>
         <div className="app">
           <MarvelCharactersList characters={displayCharacters}></MarvelCharactersList>
+          <Route exact={true} path='/' render={() => (
+            <div className="app-landing-wrapper">
+              <img className="react-logo-landing" src={logo} alt="React logo" />
+            </div>
+          )}/>
           <Route path='/character/:id' component={MarvelCharacterDetail} />
         </div>
       </Router>
--- src/components/ReactLogoLoader/ReactLogoLoader.js
+++ src/components/ReactLogoLoader/ReactLogoLoader.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './ReactLogoLoader.scss';
-import logo from './logo.svg';
+import logo from '../../assets/logo.svg';
 
 export const ReactLogoLoader = () => {
   return (<img className="react-logo-loader" src={logo} alt="React logo spinning" />);
